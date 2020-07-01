c0ba05b38a21253ec11c6fcb45e85bd596d18d84
QElderDelta <egorzhivalev@gmail.com>
2020-07-01 16:10:28 +0300

    scraped data is now being inserted into PostgreSQL database

--- scraper.py
+++ scraper.py
@@ -1,24 +1,43 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import time
+import psycopg2
+
+def insert_into_database(name, rating, year, platform):
+    try:
+        conn = psycopg2.connect(dbname='qelderdelta', 
+        user = 'qelderdelta', password = 'lolxd322', 
+        host = 'localhost')
+        cursor = conn.cursor()
+        insert_query = """ INSERT INTO games_list (name, rating, year, platform) VALUES (%s, %s, %s, %s)"""
+        to_insert = (name, rating, year, platform)
+        cursor.execute(insert_query, to_insert)
+        conn.commit()
+        cursor.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert data into database", error)
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()        
 
 if __name__ == "__main__":
-    urlPattern = "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?sort=desc&page=0"
+    url_pattern = "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?sort=desc&page="
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'    
     }
-    for i in range(2):
-        url = urlPattern + str(i)
+    for i in range(30):
+        print("Processing page #", i + 1)
+        url = url_pattern + str(i)
         request = urllib.request.Request(url, headers = headers)
         html = urllib.request.urlopen(request)
-        bsObj = BeautifulSoup(html, features="html.parser")
-        gameBlocks = bsObj.findAll("td", {"class" : "clamp-summary-wrap"})
-        for game in gameBlocks:
+        bs_obj = BeautifulSoup(html, features="html.parser")
+        game_blocks = bs_obj.findAll("td", {"class" : "clamp-summary-wrap"})
+        for game in game_blocks:
             name = game.find("a", {"class" : "title"})
             rating = game.find("div", {"class" : "metascore_w large game positive"})
             info = game.find("div", {"class" : "clamp-details"})
             year = info.find("span", recursive = False)
             platform = info.find("span", {"class" : "data"})
-            print(name.get_text() + ' ' + rating.get_text() + ' ' + ''.join(platform.get_text().strip()) 
-                + ' ' + year.get_text().split(',')[1])
+            insert_into_database(name.get_text(), rating.get_text(), year.get_text().split(',')[1], ''.join(platform.get_text().strip()))  
         time.sleep(2)          
