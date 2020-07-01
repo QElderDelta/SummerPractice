bb7721578df5a6f9d1b961d91ca06ccaaadaa422
QElderDelta <egorzhivalev@gmail.com>
2020-07-01 14:11:01 +0300

    added info about year the game was released and ability to get info from multiple pages

--- scraper.py
+++ scraper.py
@@ -1,17 +1,24 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import time
 
 if __name__ == "__main__":
-    url = "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered"
+    urlPattern = "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?sort=desc&page=0"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'    
     }
-    request = urllib.request.Request(url, headers = headers)
-    html = urllib.request.urlopen(request)
-    bsObj = BeautifulSoup(html, features="html.parser")
-    gameBlocks = bsObj.findAll("td", {"class" : "clamp-summary-wrap"})
-    for game in gameBlocks:
-        name = game.find("a", {"class" : "title"})
-        rating = game.find("div", {"class" : "metascore_w large game positive"})
-        platform = game.find("span", {"class" : "data"})
-        print(name.get_text() + ' ' + rating.get_text() + ' ' + ''.join(platform.get_text().strip()))      
+    for i in range(2):
+        url = urlPattern + str(i)
+        request = urllib.request.Request(url, headers = headers)
+        html = urllib.request.urlopen(request)
+        bsObj = BeautifulSoup(html, features="html.parser")
+        gameBlocks = bsObj.findAll("td", {"class" : "clamp-summary-wrap"})
+        for game in gameBlocks:
+            name = game.find("a", {"class" : "title"})
+            rating = game.find("div", {"class" : "metascore_w large game positive"})
+            info = game.find("div", {"class" : "clamp-details"})
+            year = info.find("span", recursive = False)
+            platform = info.find("span", {"class" : "data"})
+            print(name.get_text() + ' ' + rating.get_text() + ' ' + ''.join(platform.get_text().strip()) 
+                + ' ' + year.get_text().split(',')[1])
+        time.sleep(2)          
